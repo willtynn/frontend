b22686889732068b7912159049f14896f97b9255
yangzhiqing <yangzhiqing2001@126.com>
2023-06-29 13:27:06 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Stack direction="row" spacing={2}>
+        <Button>MUI的示例按钮</Button>
+        <Button>在/src的App.js加入Router</Button>
+        <Button href="#">其余页面通过路由访问</Button>
+      </Stack>
     </div>
   );
 }
