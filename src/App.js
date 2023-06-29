540aa257eb12e88b983157280cddb8cf45fc8f43
yangzhiqing <yangzhiqing2001@126.com>
2023-06-29 13:44:20 +0800

    add:添加代码格式化工具和配置文件，依赖版本控制文件

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import * as React from 'react';
 import Button from '@mui/material/Button';
@@ -6,11 +5,11 @@ import Stack from '@mui/material/Stack';
 
 function App() {
   return (
-    <div className="App">
-      <Stack direction="row" spacing={2}>
+    <div className='App'>
+      <Stack direction='row' spacing={2}>
         <Button>MUI的示例按钮</Button>
         <Button>在/src的App.js加入Router</Button>
-        <Button href="#">其余页面通过路由访问</Button>
+        <Button href='#'>其余页面通过路由访问</Button>
       </Stack>
     </div>
   );
