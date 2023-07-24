6f66996664f022331abadca9e0457077ecd965f7
Neil-Chao <915123030@qq.com>
2023-07-24 16:14:13 +0800

    Feat. dagre-d3绘图

--- src/Pages/Service/dependency/canvas.js
+++ src/Pages/Service/dependency/canvas.js
@@ -1,18 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import * as d3 from "d3";
 import dagreD3 from "dagre-d3";
+import { useEffect, useState } from "react";
+import { Box } from "@mui/material";
 
 import "./styles.css";
 
-export class App extends React.Component {
-  componentDidMount() {
+export function Canvas(props) {
+
+  
+
+  const [graph, setGraph] = useState(null);
+
+  const handleclick = () => {
     // Create the input graph
     var g = new dagreD3.graphlib.Graph({ compound: true })
       .setGraph({})
-      .setDefaultEdgeLabel(function() {
-        return {};
-      });
+      .setDefaultEdgeLabel(() => {});
 
     // Here we're setting the nodes
     g.setNode("a", { label: "A" });
@@ -54,7 +58,7 @@ export class App extends React.Component {
     g.setEdge("b", "f");
     g.setEdge("b", "g");
 
-    g.nodes().forEach(function(v) {
+    g.nodes().forEach(function (v) {
       var node = g.node(v);
       // Round the corners of the nodes
       node.rx = node.ry = 5;
@@ -64,11 +68,12 @@ export class App extends React.Component {
     var render = new dagreD3.render();
 
     // Set up an SVG group so that we can translate the final graph.
-    var svg = d3.select("svg"),
-      svgGroup = svg.append("g");
+    
+    var svg = d3.select(document.getElementById("svg-canvas"))
+    let svgGroup = d3.select(document.getElementById("g-canvas"))
 
     // Run the renderer. This is what draws the final graph.
-    render(d3.select("svg g"), g);
+    render(svgGroup, g);
 
     // Center the graph
     console.log(svg.attr("width"));
@@ -77,14 +82,20 @@ export class App extends React.Component {
     svgGroup.attr("transform", "translate(" + xCenterOffset + ", 20)");
     svg.attr("height", g.graph().height + 40);
   }
+  
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
-        <svg id="svg-canvas" width="800" height="600" />
-      </div>
-    );
-  }
+  return (
+    <Box 
+      sx={{
+        fontFamily: 'Open Sans',
+        textAlign: 'center'
+      }}
+    >
+      <button onClick={handleclick}>click</button>
+      <svg id="svg-canvas" className="wtf" width="800" height="600">
+        <g id="g-canvas"></g>
+      </svg>
+    </Box>
+  );
+
 }
--- src/Pages/Service/dependency/index.js
+++ src/Pages/Service/dependency/index.js
@@ -1,33 +1,61 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import DagreGraph from "dagre-d3-react";
 import { nodes, links } from "./data";
 import { Stack, Box } from "@mui/material";
-import { App } from "./app";
+import { Canvas } from "./canvas";
 
-import "./index.css";
+const data = {
+  invoked: [
+    {
+      id: "service_a",
+      invoke_info: {
+        time: "2023-07-24 16:00:00"
+      }
+    },
+    {
+      id: "service_b",
+      invoke_info: {
+        time: "2023-07-24 16:10:00"
+      }
+    }
+  ],
+  invoking: [
+    {
+      id: "service_c",
+      invoke_info: {
+        time: "2023-07-24 16:20:00"
+      }
+    },
+    {
+      id: "service_d",
+      invoke_info: {
+        time: "2023-07-24 16:30:00"
+      }
+    }
+  ]
+}
+
+
 
 function ServiceDependency() {
+
+  const transformData = (data) => {
+    let nodes = []
+    let links = []
+    nodes.push(data.invoked.concat(data.invoking).map(
+      (item, index) => {
+        return {
+          id: item.id,
+          label: item.id
+        }
+      }
+    ))
+  }
+
   return (
-    <Stack>
-    <div className="App">
-      <DagreGraph
-        nodes={nodes}
-        links={links}
-        rankdir="TB"
-        width="800"
-        height="1200"
-        animate={1000}
-        shape="circle"
-        fitBoundaries
-        onNodeClick={e => console.log(e)}
-        onRelationshipClick={e => console.log(e)}
-      />
-    </div>
     <Box>
-      <App></App>
+      <Canvas />
     </Box>
-    </Stack>
   );
 }
 
