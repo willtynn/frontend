2beb20b34cddab9f5918b509dc361733dfc75871
Neil-Chao <915123030@qq.com>
2023-08-02 19:37:59 +0800

    Feat. 修改application/json

--- src/utils/axios_instance.js
+++ src/utils/axios_instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getLocalStorage } from '@/utils/localStorageHandler';
 
-axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
+axios.defaults.headers['Content-Type'] = 'application/json';
 
 export const baseURLLink = "http://192.168.1.104:31931";
 console.log('Now using base link: ', baseURLLink);
