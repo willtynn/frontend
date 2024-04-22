f7d7f5a989c326595be57e82f70ff4902d7455ea
Daniel <2116528714@qq.com>
2024-04-22 17:34:36 +0800

    Feat. 修改路由链路的加载效果（删除全屏遮罩）

--- src/Pages/Route/trace/index.js
+++ src/Pages/Route/trace/index.js
@@ -26,6 +26,7 @@ import {
   Popover,
 } from '@mui/material';
 import Question from '@/assets/Question.svg';
+import CircularProgress from '@mui/material/CircularProgress';
 import Link60 from '@/assets/Link60.svg';
 import {
   KubeConfirmButton,
@@ -44,7 +45,6 @@ import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import { fontFamily } from '@/utils/commonUtils';
 import { ServiceRow } from './ServiceRow';
-import { Loading } from './Loading';
 import dayjs from 'dayjs';
 
 import {
@@ -412,7 +412,7 @@ export default function RouteTrace() {
   //#region
   return (
     <>
-      <Loading show={showLoading} />
+      {/* {<Loading show={showLoading} />} */}
 
       <Popover
         id='route-trace-table-content-popover'
@@ -675,7 +675,8 @@ export default function RouteTrace() {
                               fontStyle: 'normal',
                             }}
                           >
-                            <Question />
+                            {showLoading ? <CircularProgress /> : <Question />}
+                            
                             <NormalBoldFont>{intl.messages['routeTrace.serviceTableContentNoData']}</NormalBoldFont>
 
                             <SmallLightFont>{intl.messages['routeTrace.serviceTableContentNoDataHint']}</SmallLightFont>
