a5c1e7bfd7a3a06dacccfd88defd09d1af4cdb82
Neil-Chao <915123030@qq.com>
2024-06-05 12:10:35 +0800

    feat. 压测后端路径开发

--- src/actions/applicationAction.js
+++ src/actions/applicationAction.js
@@ -74,8 +74,8 @@ export const UPDATE_AGGREGATE_REPORT = 'UPDATE_AGGREGATE_REPORT';
 export const UPDATE_CHANGE_FLAG = 'UPDATE_CHANGE_FLAG';
 export const UPDATE_START_AND_END = 'UPDATE_START_AND_END';
 
-// const baseURLLink = 'http://192.168.1.104:14447';
-const baseURLLink = 'http://localhost:8848';
+const baseURLLink = 'http://192.168.1.104:14447';
+// const baseURLLink = 'http://localhost:8848';
 
 const axios_instance = axios.create({
   baseURL: baseURLLink,
